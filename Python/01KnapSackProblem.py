89cf4dd7107ab5c87d8e92fe1c347159cdd32ef0
Yogesh Sundaresan <syogesh0705@gmail.com>
2017-08-25 23:31:56 +0530

    KnapSackProblem - py added

--- Python/01KnapSackProblem.py
+++ Python/01KnapSackProblem.py
@@ -3,17 +3,17 @@ http://www.geeksforgeeks.org/knapsack-problem/
 """
 
 
-def knapsack_01(v, w, n, tot):
-    t = [[1 for c in range(tot + 1)] for r in range(n + 1)]
+def knapsack_01(val, wt, n, w):
+    t = [[1 for c in range(w + 1)] for r in range(n + 1)]
     for i in range(n + 1):
-        for j in range(tot + 1):
+        for j in range(w + 1):
             if i == 0 or j == 0:
                 t[i][j] = 0
-            elif j < w[i - 1]:
+            elif j < wt[i - 1]:
                 t[i][j] = t[i - 1][j]
             else:
-                t[i][j] = max(v[i - 1] + t[i][j - w[i - 1]], t[i - 1][j])
-    return t[n][tot]
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j])
+    return t[n][w]
 
 
 # Driver function to test the above function
@@ -22,7 +22,7 @@ def driver():
     wt = [10, 20, 30]
     w = 50
     n = len(val)
-    print(knapsack_01(w, wt, val, n))
+    print(knapsack_01(val, wt, n, w))
 
 
 driver()
